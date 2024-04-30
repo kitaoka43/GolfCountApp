07a9d25bf043064aad3626f23de4f61a211b93d2
kitaoka43 <kitaoka.hayato1998@gmail.com>
2024-05-01 01:44:00 +0900

    Initial Commit

---
+++ GolfCountApp Watch App/GolfCountAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfCountAppApp.swift
+//  GolfCountApp Watch App
+//
+//  Created by 北岡勇人 on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct GolfCountApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
